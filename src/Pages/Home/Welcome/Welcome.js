591f1438807c69e4338d939bc1730ee2a0a84b4a
rifatjahan823 <rifatjahan823@gmail.com>
2022-04-30 05:12:12 +0600

    add fake data for fontend

--- src/Pages/Home/Welcome/Welcome.js
+++ src/Pages/Home/Welcome/Welcome.js
@@ -14,13 +14,13 @@ const Welcome = () => {
             <div className="container">
             <div className="row justify-content-end align-items-center g-5">
             {/*--- welcome-content area ----  */}
-            <div className="welcome-content col-md-6 col-sm-12 pe-5">
+            <div className="welcome-content col-md-6 col-sm-12  order-lg-1 order-md-1 order-2">
             <h1>best car WareHouse</h1>
             <h2>WELCOME TO AUTOSTARS LISTING PORTAL</h2>
             <p className='pe-5'>CarWorld is the world's leading portal for easy and quick car buying and selling</p>
             </div>
             {/*--- welcome-Image area ----  */}
-            <div className="welcome-image col-md-6 col-sm-12 ">
+            <div className="welcome-image col-md-6 col-sm-12 order-lg-2 order-md-2 order-1">
            <div className='shape'>
            <img  src={shape} alt="" />
            </div>
